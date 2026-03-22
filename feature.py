13b8dc8d86f0a9ae26c9a4040e7bbdb1e735978e
shnalpavel2019 <shnalpavel2019@gmail.com>
2026-03-22 14:15:14 +0200

    feature

--- feature.py
+++ feature.py
@@ -1,7 +1,7 @@
 class Feature:
     def __init__(self, index, description):
-        self.Id = index
-        self.Description = description
+        self.id = index
+        self.description = description
 
     def __str__(self):
-        return "Feature #" + str(self.Id) + ": " + self.Description
+        return "Feature #" + str(self.id) + ": " + self.description
